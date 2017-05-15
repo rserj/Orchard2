e12f1a7fcaa81a016520304fc75976475a99a201
Nicholas Mayne <Jetski5822@gmail.com>
2017-05-15 23:59:21 +0100

    adding initial client

---
+++ src/Orchard.ThirdParty/GoogleCloud/Orchard.GoogleCloud.Diagnostics.Trace/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Google.Cloud.Trace.V1;
+using Microsoft.AspNetCore.Modules;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Orchard.GoogleCloud.Diagnostics.Trace
+{
+    public class Startup : StartupBase
+    {
+        public override void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<TraceServiceClient>(_ => {
+                return Google.Cloud.Trace.V1.TraceServiceClient.Create();
+            });
+        }
+    }
+}
