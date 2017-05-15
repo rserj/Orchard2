0d67d683ffa2ebd52d8c120bf46a62a98e20c359
Nicholas Mayne <Jetski5822@gmail.com>
2017-05-16 00:14:48 +0100

    Trace Start and stop

--- src/Orchard.ThirdParty/GoogleCloud/Orchard.GoogleCloud.Diagnostics.Trace/Startup.cs
+++ src/Orchard.ThirdParty/GoogleCloud/Orchard.GoogleCloud.Diagnostics.Trace/Startup.cs
@@ -1,6 +1,11 @@
-﻿using Google.Cloud.Trace.V1;
+﻿using System;
+using System.Diagnostics;
+using Google.Cloud.Trace.V1;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Modules;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DiagnosticAdapter;
 
 namespace Orchard.GoogleCloud.Diagnostics.Trace
 {
@@ -11,6 +16,27 @@ namespace Orchard.GoogleCloud.Diagnostics.Trace
             services.AddSingleton<TraceServiceClient>(_ => {
                 return Google.Cloud.Trace.V1.TraceServiceClient.Create();
             });
+        }
+
+        public override void Configure(IApplicationBuilder app, IRouteBuilder routes, IServiceProvider serviceProvider)
+        {
+            var diagnosticListener = serviceProvider.GetRequiredService<DiagnosticListener>();
+            diagnosticListener.SubscribeWithAdapter(new GoogleCloudTraceListener());
+        }
+    }
+
+    public class GoogleCloudTraceListener
+    {
+        [DiagnosticName("Trace.Starting")]
+        public virtual void OnTraceStarting()
+        {
+        }
+
+        [DiagnosticName("Trace.Finished")]
+        public virtual void OnTraceFinished()
+        {
+            var r = new Google.Cloud.Trace.V1.Trace { };
+            r.Spans.Add(new TraceSpan {  });
         }
     }
 }
