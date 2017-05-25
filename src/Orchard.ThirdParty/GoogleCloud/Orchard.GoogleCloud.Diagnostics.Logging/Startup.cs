4a54708e347b7267e6c4c4050426c17dee8fbbb2
Nicholas Mayne <Jetski5822@gmail.com>
2017-05-25 15:41:50 +0100

    Adding logging at the tenant.

--- src/Orchard.ThirdParty/GoogleCloud/Orchard.GoogleCloud.Diagnostics.Logging/Startup.cs
+++ src/Orchard.ThirdParty/GoogleCloud/Orchard.GoogleCloud.Diagnostics.Logging/Startup.cs
@@ -2,20 +2,27 @@
 using Google.Cloud.Diagnostics.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Modules;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Orchard.Environment.Shell;
 
 namespace Orchard.GoogleCloud.Diagnostics.Logging
 {
-    public static class Log4NetExtensions
+    public class Startup : StartupBase
     {
-        public static Microsoft.Extensions.Logging.ILoggerFactory AddLog4Net(
-            this Microsoft.Extensions.Logging.ILoggerFactory loggerFactory,
-            IApplicationBuilder app)
-        {
-            // https://github.com/GoogleCloudPlatform/google-cloud-dotnet/blob/97ddf80e840b02539c5d1e3a8f1b1efa1499fefc/apis/Google.Cloud.Diagnostics.AspNetCore/Google.Cloud.Diagnostics.AspNetCore/Logging/GoogleLoggerFactoryExtensions.cs
+        private readonly GoogleShellSettings _googleShellSettings;
 
-            return null;
+        public Startup(
+            ShellSettings shellSettings)
+        {
+            _googleShellSettings = new GoogleShellSettings(shellSettings);
+        }
+
+        public override void Configure(IApplicationBuilder app, IRouteBuilder routes, IServiceProvider serviceProvider)
+        {
+            var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+            loggerFactory.AddGoogle(_googleShellSettings.ProjectId);
         }
     }
 }
